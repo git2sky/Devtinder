7d7720acaf78ff78334f7f3a1ca032e4141d0fdd
Sanjeev Kumar Yadav <syadav@zlti.com>
2025-02-15 19:53:52 +0530

    Updated edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -22,7 +22,10 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
         const loggedInUser = req.user;
         Object.keys(req.body).forEach(key => loggedInUser[key] = req.body[key]);
         await loggedInUser.save();
-        res.send(`${loggedInUser.firstName}, yours profile has been updated Successfull`);
+        res.json({
+            message : `${loggedInUser.firstName}, yours profile has been updated Successfull`,
+            data : loggedInUser
+        });
     } catch (err) {
         res.status(400).send("Error : " + err.message)
     }
