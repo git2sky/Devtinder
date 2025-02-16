f4c719d991a852adfd9e66b6617cbd314d3d2aae
Sanjeev Kumar Yadav <syadav@zlti.com>
2025-02-16 10:58:41 +0530

    Created connectionRequest API

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const requestRouter = express.Router();
-const {userAuth} = require('../middlewares/userAuth.js');
+const { userAuth } = require('../middlewares/userAuth.js');
+const ConnectionRequest = require('../models/connectionRequests.js');
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req,res)=>{
-    const user = req.user;
-    console.log(user.firstName +" sending conn req");
-    res.status(200).send("Connection req sent");
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try {
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+        const allowedStatus = ["interested" , "ignored"];
+
+        if(!allowedStatus.includes(status)){
+           return res.status(400).send("Invalid status type : "+ status)
+        }
+
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+        const data = await connectionRequest.save();
+        res.json({
+            message : "Connection request sent successfully!",
+            data
+        });
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
 });
 
 module.exports = requestRouter;
