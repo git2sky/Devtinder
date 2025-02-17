620c516e122f72f249b125314ca166c2427ad5f4
Sanjeev Kumar Yadav <syadav@zlti.com>
2025-02-18 00:47:26 +0530

    created connection Review API

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -48,4 +48,35 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     }
 });
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const { status, requestId } = req.params;
+        const allowedStatus = ["accepted", "rejected"];
+
+        if (!allowedStatus.includes(status)) {
+            return res.status(400).send("Invalid status type : " + status)
+        }
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            status: 'interested',
+            toUserId: loggedInUser._id
+        })
+
+        if (!connectionRequest) {
+            return res.status(404).send("Connection request not found");
+        }
+
+        connectionRequest.status = status;
+
+        const data = await connectionRequest.save();
+        res.json({
+            message: "Connection request "+ status,
+            data
+        });
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
 module.exports = requestRouter;
