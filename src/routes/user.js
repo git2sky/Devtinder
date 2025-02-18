e0b3b42c4c7d60d3d91f58459c2da25fdb620049
Sanjeev Kumar Yadav <syadav@zlti.com>
2025-02-18 11:45:18 +0530

    Added User connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const {userAuth} = require('../middlewares/userAuth');
 const ConnectionRequest = require('../models/connectionRequests.js');
 const userRouter = express.Router();
 
+const SAFE_USER_DATA = ["firstName", "lastName", "age", "skills", "gender"];
+
 userRouter.get('/user/requests/received', userAuth, async (req,res)=>{
     try{
         const loggedInUser = req.user;
@@ -17,6 +19,28 @@ userRouter.get('/user/requests/received', userAuth, async (req,res)=>{
         });
 
     }catch(err){
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
+userRouter.get('/user/connections', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequestsData = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: 'accepted' },
+                { fromUserId: loggedInUser._id, status: 'accepted' }
+            ]
+        }).populate("fromUserId", SAFE_USER_DATA).populate("toUserId", SAFE_USER_DATA);
+
+        const data = connectionRequestsData.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+        res.json({ data });
+    } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
 });
