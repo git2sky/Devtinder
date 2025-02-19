22d8b81210b421306cf711c2c3c0288b2c612bdd
Sanjeev Kumar Yadav <syadav@zlti.com>
2025-02-20 00:27:07 +0530

    Added pagination in feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -48,6 +48,11 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 userRouter.get('/feed', userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.page) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1)* limit;
+
         const connectionRequestsData = await ConnectionRequest.find({
             $or: [
                 { toUserId: loggedInUser._id },
@@ -66,8 +71,9 @@ userRouter.get('/feed', userAuth, async (req, res) => {
                 {_id: { $nin : Array.from(hiddenUsersFromFeed) } },
                 {_id: { $ne : loggedInUser._id } }
             ]
-        }).select(SAFE_USER_DATA);
-        res.send(users)
+        }).select(SAFE_USER_DATA).skip(skip).limit(limit);
+
+        res.json({data : users});
     } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
